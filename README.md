df74830486628408346af797baae416b62d5dcbb
Continuous Integration <ci@example.com>
2019-10-18 08:09:14 +0000

    Update gh-pages automatically

---
+++ README.md
@@ -0,0 +1,8 @@
+# Klarna Payments Integration Guide
+
+This project shows you how to integrate with Klarna Payments. For a demo, please check out [here](https://hex0cter.github.io/klarna-payments-integration-guide/).
+
+## References
+* https://developers.klarna.com/documentation/klarna-payments/integration-guide/
+* https://developers.klarna.com/api/
+* https://developers.klarna.com/resources/order-management/
---
+++ precache-manifest.04465b2cd422f5af3e3e0004efc07613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e414b2ad4d41adfb699b577d4fd92844",
+    "url": "/klarna-payments-integration-guide/index.html"
+  },
+  {
+    "revision": "b800f97f05491af331ad",
+    "url": "/klarna-payments-integration-guide/static/css/2.b61b8460.chunk.css"
+  },
+  {
+    "revision": "8a9bcc0ee104f15600b9",
+    "url": "/klarna-payments-integration-guide/static/css/main.62efec46.chunk.css"
+  },
+  {
+    "revision": "b800f97f05491af331ad",
+    "url": "/klarna-payments-integration-guide/static/js/2.68ddbcd7.chunk.js"
+  },
+  {
+    "revision": "8a9bcc0ee104f15600b9",
+    "url": "/klarna-payments-integration-guide/static/js/main.593df725.chunk.js"
+  },
+  {
+    "revision": "55cdd41dcce9334f79c3",
+    "url": "/klarna-payments-integration-guide/static/js/runtime~main.19791e9f.js"
+  }
+]);
